7f0dd6313eff3dd8f7031368b6a5506dba227fce
trumpocalyspe <mark.jn.kim@gmail.com>
2018-08-12 15:31:52 -0700

    added server.js express route

---
+++ routes/apiRoutes.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+const router = require("express").Router;
+
+router.get("/articles", (req, res) => {
+  axios
+  .get("https://www.nytimes.com/", { params: req.query })
+  .then(({ data: { results} }) => res.json(results))
+  .catch(err => res.status(422).json(err));
+
+});
+
+module.exports =router;
