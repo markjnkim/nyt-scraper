5b61d9b91c52e421fbac341b35f0f73b0683f45c
trumpocalyspe <mark.jn.kim@gmail.com>
2018-09-17 14:40:35 -0700

    added Article model

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("*", function(req, res) {
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
+  process.env.MONGODB_URI || "mongodb://localhost/nytreact"
 );
 
 app.listen(PORT, function() {
