3683472d1bc628af753efe936b3d8eb77d7cf61f
trumpocalyspe <mark.jn.kim@gmail.com>
2018-09-18 13:18:30 -0700

    added routes

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+// API Routes
+router.use("/api", apiRoutes);
+
+// If no API routes are hit, send the React App
+router.use((req, res) => {
+  res.send("Welcome to REACT")
+  // res.sendFile(path.join(__dirname, "../client/public/index.html"))
+});
+
+module.exports = router;
